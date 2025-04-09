510be6052e77c04254d00913e8ca6c7981bea40b
DJ STUDIO <128752386+DJOPRO-STUDIO@users.noreply.github.com>
2025-04-09 16:17:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def home(id):
   if a and a.data[0].state == "Completed":
     return a.data[0].imageUrl
   return ""
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
